82e91898d18e514db0ffd3640ee033c47d90fcf6
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-09-05 09:59:27 +0200

    Descrizione delle modifiche

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MenuPageComponent } from './pages/menu-page/menu-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: LandingPageComponent},
+    { path: "menu", component: MenuPageComponent }
+];
